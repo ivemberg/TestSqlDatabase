2bd16f9308b46ea7fcf574c683710b9f1a829781
Paolo ferrari <paoloferrariapple@gmail.com>
2022-06-30 14:46:34 +0200

    Initial Commit

---
+++ TestSqlDatabase/TestSqlDatabaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestSqlDatabaseApp.swift
+//  TestSqlDatabase
+//
+//  Created by user on 30/06/22.
+//
+
+import SwiftUI
+
+@main
+struct TestSqlDatabaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
